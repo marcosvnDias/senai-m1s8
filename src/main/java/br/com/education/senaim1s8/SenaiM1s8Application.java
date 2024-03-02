946ea6395923da129b353f4f9ae2efaa7588105f
Marcos Vinicios <mvdias1268@gmail.com>
2024-03-02 20:44:53 -0300

    first commit

---
+++ src/main/java/br/com/education/senaim1s8/SenaiM1s8Application.java
@@ -0,0 +1,13 @@
+package br.com.education.senaim1s8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiM1s8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiM1s8Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/education/senaim1s8/SenaiM1s8ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.education.senaim1s8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiM1s8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
