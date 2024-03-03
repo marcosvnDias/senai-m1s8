c891b5e6e10a1904812b49731ce3b7dc26dd7f37
Marcos Vinicios <mvdias1268@gmail.com>
2024-03-02 22:48:57 -0300

    Correção no chamado da função getCursos dentro do CursoService

--- src/main/java/br/com/education/senaim1s8/service/CursoService.java
+++ src/main/java/br/com/education/senaim1s8/service/CursoService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class CursoService {
     public List<CursoModel> listarCursos () {
-        return CursoModel.getAlunos();
+        return CursoModel.getCursos();
     }
 
     public CursoModel cadastrar (CursoModel curso) {
